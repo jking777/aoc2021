1248a8643f4fae2e7c6d59d1ff58aac0cffd44e5
Jim King <jim.king@bluware.com>
2021-12-06 08:41:10 -0600

    add elapsed time display

--- kotlin/AdventOfCode2021/src/main/kotlin/Day6.kt
+++ kotlin/AdventOfCode2021/src/main/kotlin/Day6.kt
@@ -5,6 +5,8 @@ class Day6 {
 //        parseInput("..\\..\\data\\day6_sample.txt")
         parseInput("..\\..\\data\\day6.txt")
 
+        var startMillis = System.currentTimeMillis()
+
         var ages: MutableList<Int> = ArrayList(initialAges)
 
         for (day in 1..80) {
@@ -26,7 +28,12 @@ class Day6 {
             }
         }
 
+        val elapsed1 = System.currentTimeMillis() - startMillis
         println("Day 6 Part 1    Number of lantern fish after 80 days:  ${ages.size}")
+        println("Day 6 Part 1    Time:  $elapsed1 ms")
+        println()
+
+        startMillis = System.currentTimeMillis()
 
         var ageGroups: MutableMap<Int, Long> = HashMap()
         for (age in initialAges) {
@@ -59,7 +66,11 @@ class Day6 {
         }
 
         val fishes = ageGroups.map { it.value }.sum()
+
+        val elapsed2 = System.currentTimeMillis() - startMillis
         println("Day 6 Part 2    Number of lantern fish after 256 days:  $fishes")
+        println("Day 6 Part 2    Time:  $elapsed2 ms")
+        println()
     }
 
     fun parseInput(filename: String) {
