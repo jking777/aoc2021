c8e0448515ca2615c5c26dae5760fea1fe52597f
Jim King <jim@jimking.net>
2021-12-06 20:15:25 -0600

    day 7 stubs

---
+++ kotlin/AdventOfCode2021/src/main/kotlin/Day7.kt
@@ -0,0 +1,11 @@
+class Day7 {
+    fun run() {
+        parseInput("..\\..\\data\\day7_sample.txt")
+//        parseInput("..\\..\\data\\day7.txt")
+
+    }
+
+    fun parseInput(filename: String) {
+
+    }
+}
