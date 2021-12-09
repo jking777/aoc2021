db01d88ff5221823594c019106cb1addf27d0873
Jim King <jim@jimking.net>
2021-12-08 20:57:37 -0600

    day 9 stubs

---
+++ kotlin/AdventOfCode2021/src/main/kotlin/Day9.kt
@@ -0,0 +1,11 @@
+class Day9 {
+    fun run() {
+        parseInput(1)
+    }
+
+    private fun parseInput(sample: Int) {
+        val filename =
+            if (sample <= 0) "..\\..\\data\\day9.txt" else "..\\..\\data\\day9_sample.txt"
+
+    }
+}
--- kotlin/AdventOfCode2021/src/main/kotlin/Main.kt
+++ kotlin/AdventOfCode2021/src/main/kotlin/Main.kt
@@ -5,5 +5,6 @@ fun main(args: Array<String>) {
 //    Day5().run()
 //    Day6().run()
 //    Day7().run()
-    Day8().run()
+//    Day8().run()
+    Day9().run()
 }
