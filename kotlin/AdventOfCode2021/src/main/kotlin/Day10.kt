4c3745d5eb8d5ab58791c91f3c00de62a27a687e
Jim King <jim.king@bluware.com>
2021-12-09 22:08:50 -0600

    day 10 stubs

---
+++ kotlin/AdventOfCode2021/src/main/kotlin/Day10.kt
@@ -0,0 +1,11 @@
+class Day10 {
+    fun run() {
+        parseInput(1)
+    }
+
+    private fun parseInput(sample: Int) {
+        val filename =
+            if (sample <= 0) "..\\..\\data\\day10.txt" else "..\\..\\data\\day10_sample.txt"
+
+    }
+}
