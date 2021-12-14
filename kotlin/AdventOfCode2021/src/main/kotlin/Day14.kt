e0ac7c4b501fdc0bd9bdf5489097ad7b19f56adf
Jim King <jim.king@bluware.com>
2021-12-14 17:02:36 -0600

    really bad solution to day 14 part 2

--- kotlin/AdventOfCode2021/src/main/kotlin/Day14.kt
+++ kotlin/AdventOfCode2021/src/main/kotlin/Day14.kt
@@ -2,13 +2,45 @@ import java.io.File
 
 class Day14 {
     fun run() {
-        parseInput(1)
+        parseInput(0)
 
         val result1 = doIt(10)
         println("Day 14 Part 1    most - least:  $result1")
 
-        val result2 = doIt(20)
-        println("Day 14 Part 2    most - least:  $result2")
+        // Part 2 is cribbed from looking at the Reddit solutions thread
+
+        var pairs: MutableMap<Pair<Char, Char>, Long> = HashMap()
+        for (i in 0 until startingTemplate.size - 1) {
+            val key = Pair(startingTemplate[i], startingTemplate[i + 1])
+            pairs[key] = pairs.getOrDefault(key, 0) + 1
+        }
+
+        for (step in 1..40) {
+            val newPairs: MutableMap<Pair<Char, Char>, Long> = HashMap()
+            for (p in pairs) {
+                val sub = substitutions[p.key]!!
+                val p1 = Pair(p.key.first, sub)
+                val p2 = Pair(sub, p.key.second)
+                newPairs[p1] = newPairs.getOrDefault(p1, 0) + p.value
+                newPairs[p2] = newPairs.getOrDefault(p2, 0) + p.value
+            }
+            pairs = newPairs
+        }
+
+        val letterCounts: MutableMap<Char, Long> = HashMap()
+        for (p in pairs) {
+            letterCounts[p.key.first] = letterCounts.getOrDefault(p.key.first, 0) + p.value
+            letterCounts[p.key.second] = letterCounts.getOrDefault(p.key.second, 0) + p.value
+        }
+
+        // first and last letters need a bump
+        letterCounts[startingTemplate.first()] = letterCounts[startingTemplate.first()]!! + 1
+        letterCounts[startingTemplate.last()] = letterCounts[startingTemplate.last()]!! + 1
+
+        val mostCommon = letterCounts.maxOf { it.value }
+        val leastCommon = letterCounts.minOf { it.value }
+
+        println("Day 14 Part 2    most - least:  ${mostCommon/2 - leastCommon/2}")
     }
 
     private fun doIt(count: Int): Int {
